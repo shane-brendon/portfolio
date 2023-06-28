13181498e529bf6d12881575a38f13011345e5d9
shane <shane@extension-interactive.com>
2023-06-28 12:33:26 +0400

    [+][UPD][Resources] - feature/setFolder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from '../app/components/header/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Header/>
       <body className={inter.className}>{children}</body>
     </html>
   )
