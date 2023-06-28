f095109082f1939be9c70e1d1b1cbf1484a9b7f7
shane <shane@extension-interactive.com>
2023-06-28 12:38:26 +0400

    Revert "[+][UPD][Resources] - feature/setFolder"
    This reverts commit 13181498e529bf6d12881575a38f13011345e5d9.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Header from '../app/components/header/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Header/>
       <body className={inter.className}>{children}</body>
     </html>
   )
