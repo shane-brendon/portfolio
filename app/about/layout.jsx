ad8ba1b25d844ae2b55fa31ce5f339d42ec88881
shane <shane@extension-interactive.com>
2023-06-28 10:08:37 +0400

    [+][UPD][Resources] - feature/Aboutpage

---
+++ app/about/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const aboutLayout = ({children}) => {
+  return (
+    <div>
+        <h1>hello world</h1>
+        {children}
+    </div>
+  )
+}
+
+export default aboutLayout
