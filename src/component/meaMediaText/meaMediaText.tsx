1813038fe610eafe119300a17c2231dcb8d7f4ff
shane <shane@extension-interactive.com>
2023-07-19 19:15:47 +0400

    [+][UPD][Resources] - feature / meaMediaText-desktop

--- src/component/meaMediaText/meaMediaText.tsx
+++ src/component/meaMediaText/meaMediaText.tsx
@@ -9,21 +9,23 @@ const meaMediaText = () => {
         <div className="meaMediaText__top">
           <img src="/meaMediaText.png" alt="" />
           <div className="meaMediaText__content">
-            <div className="caption">About</div>
-            <h3 className="meaMediaText__content--title">
-              An Experience Design Agency
-            </h3>
-            <div className="meaMediaText__content--subTitle">
-              Provides a full service range
+            <div className="meaMediaText__content--wrapper">
+              <div className="caption">About</div>
+              <h3 className="meaMediaText__content--title">
+                An Experience Design Agency
+              </h3>
+              <div className="meaMediaText__content--subTitle">
+                Provides a full service range
+              </div>
+              <div className="description-dark">
+                Ability to put themselves in the merchant's shoes. It is meant
+                to partner on the long run, and work as an extension of the
+                merchant's team.
+              </div>
+              <Link href="#" className="meaMediaText__content--btn">
+                ABOUT US
+              </Link>
             </div>
-            <div className="description-dark">
-              Ability to put themselves in the merchant's shoes. It is meant to
-              partner on the long run, and work as an extension of the
-              merchant's team.
-            </div>
-            <Link href="#" className="meaMediaText__content--btn">
-              ABOUT US
-            </Link>
           </div>
         </div>
         <div className="meaMediaText__bottom">
