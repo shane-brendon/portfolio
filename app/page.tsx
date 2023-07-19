408b2b2290b1963d27cf1d4ff531d4abf1205086
shane <shane@extension-interactive.com>
2023-07-19 14:57:28 +0400

    [+][UPD][Resources] - feature / section-logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero from '@/src/component/hero/hero'
-
+import Logo from '@/src/component/logo/logo'
 export default function Home() {
   return (
     <>
       <Hero/>
+      <Logo/>
     </>
   )
 }
